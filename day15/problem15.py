60ab14b04d40e5f6333fa2d13a62d4f4d6f5bfa9
Cheryl Danner <sheromon@users.noreply.github.com>
2019-12-15 16:31:23 -0500

    Added my solution for day 15, part b.

--- day15/problem15.py
+++ day15/problem15.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from intcode import Computer
 
+# if origin is 31, 31, oxygen is at 15, 19
 
 def problem15a():
     file_name = 'problem15.txt'
@@ -27,7 +28,58 @@ def problem15a():
                 print("Found oxygen!")
                 print("Oxygen is at", droid.coords)
                 print("Num steps:", len(droid.history))
-                return droid
+                return droid, droid.coords
+
+
+def problem15b():
+    # get to the oxygen!
+    droid, oxygen_coords = problem15a()
+    droid.reset_map()
+    droid.display_map()
+
+    # these are counters for current/max number of steps from oxygen
+    num_steps = 0
+    max_steps = 0
+    # this will help retrace steps after hitting a dead end
+    good_directions_history = []
+
+    while not droid.computer.done:
+        directions, vals = droid.get_next_directions()
+        unexplored_inds = vals == -1
+        directions = directions[unexplored_inds]
+        good_directions = []
+        for direction in directions:
+            droid.send_command(direction)
+            output = droid.computer.run()
+            if output is None:
+                print("Program completed")
+            droid.update(direction, output)
+
+            if output > 0:
+                if output == 2 or np.all(droid.coords == oxygen_coords):
+                    print("Found oxygen!")
+                    num_steps = 0  # reset counter for steps from oxygen
+                good_directions.append(direction)
+                droid.retrace()
+        if (directions.size == 0) or not good_directions:
+            if num_steps > max_steps:
+                print("Updated max steps to", num_steps)
+                max_steps = num_steps
+            good_directions = []
+            while not good_directions:
+                if num_steps == 0:
+                    print("Done exploring!")
+                    return
+                droid.retrace()
+                good_directions = good_directions_history.pop()
+                num_steps -= 1
+        selected_direction = good_directions.pop()
+        droid.send_command(selected_direction)
+        output = droid.computer.run()
+        droid.update(selected_direction, output)
+        droid.display_map()
+        good_directions_history.append(good_directions)
+        num_steps += 1
 
 
 class Droid():
@@ -44,10 +96,13 @@ class Droid():
             'oxygen': 2,
         }
         self.map = self.map_codes['unexplored'] * np.ones((map_size, map_size), dtype=np.int8)
-        self.origin = map_size // 2 + 1 * np.ones(2, dtype=np.int32)
-        self.coords = np.copy(self.origin)
+        self.coords = map_size // 2 + 1 * np.ones(2, dtype=np.int32)
         self.map[self.coords[0], self.coords[1]] = self.map_codes['droid']
         self.fig = plt.figure()  # for displaying map
+        self.history = []
+
+    def reset_map(self):
+        self.map[:] = self.map_codes['unexplored']
         self.history = []
 
     def get_retrace_direction(self, prev_direction):
@@ -114,3 +169,4 @@ class Droid():
 
 if __name__ == '__main__':
     problem15a()
+    problem15b()
